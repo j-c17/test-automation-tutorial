2a649668d485ffb8c79c2210b09f4f8bc2e28dbe
iphbl <iph.black.bear@outlook.com>
2020-10-08 18:16:26 +0200

    add html report

--- src/main/java/client/web/SeleniumDriverManager.java
+++ src/main/java/client/web/SeleniumDriverManager.java
@@ -1,12 +1,20 @@
 package client.web;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SeleniumDriverManager {
+
+    private static final Logger LOG = LogManager.getLogger(SeleniumDriverManager.class);
+    private static final ThreadLocal<RemoteWebDriver> localDrivers = new ThreadLocal<>();
+    private static final List<RemoteWebDriver> drivers = new ArrayList<>();
 
     private static final String CHROME_DRIVER_NAME = "chromedriver";
     private static final String FIREFOX_DRIVER_NAME = "geckodriver";
@@ -30,10 +38,31 @@ public class SeleniumDriverManager {
     }
 
     public static RemoteWebDriver getChromeDriver() {
-        return new ChromeDriver();
+        ChromeDriver driver = new ChromeDriver();
+        localDrivers.set(driver);
+        drivers.add(driver);
+        return driver;
     }
 
     public static RemoteWebDriver getFirefoxDriver() {
-        return new FirefoxDriver();
+        FirefoxDriver driver = new FirefoxDriver();
+        localDrivers.set(driver);
+        drivers.add(driver);
+        return driver;
+    }
+
+    public static RemoteWebDriver getCurrentDriver() {
+        return localDrivers.get();
+    }
+
+    public static void closeAllDrivers() {
+        localDrivers.remove();
+        for (RemoteWebDriver driver : drivers) {
+            try {
+                driver.quit();
+            } catch (Exception ex) {
+                LOG.error(ex.getMessage());
+            }
+        }
     }
 }
--- src/main/java/utils/TestInitialization.java
+++ src/main/java/utils/TestInitialization.java
@@ -1,6 +1,11 @@
 package utils;
 
 import client.web.SeleniumDriverManager;
+import io.restassured.RestAssured;
+import io.restassured.internal.RestAssuredResponseImpl;
+import io.restassured.response.Response;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.nio.file.Paths;
@@ -8,6 +13,8 @@ import java.nio.file.Paths;
 public class TestInitialization {
 
     private static boolean isInit;
+
+    private static final Logger LOG = LogManager.getLogger(TestInitialization.class);
 
     static {
         isInit = false;
@@ -27,10 +34,28 @@ public class TestInitialization {
                 String env = TestConfiguration.getEnv();
                 TestConfiguration.readConfig(Paths.get(userDir, "env_config", env + ".properties"));
 
+                TestReport.init();
+                TestReport testReport = new TestReport();
+
                 SeleniumDriverManager.setChromeDriverPath(Paths.get(userDir, "drivers", SeleniumDriverManager.getChromeDriverName()));
 
-                isInit = true;
+                RestAssured.filters((request, response, filterContext) -> {
+                    //request logging
+                    String requestMsg = "-->  Request " + request.getMethod() + " " + request.getURI();
 
+                    Response next = filterContext.next(request, response);
+                    String responseBody = ((RestAssuredResponseImpl) next).getBody().asString().replace("\n", " ").replace(" ", "");
+                    String responseMsg = "<-- Response " + next.getStatusCode() + " " + responseBody;
+
+                    LOG.info(requestMsg);
+                    LOG.info(responseMsg);
+                    testReport.log(requestMsg);
+                    testReport.log(responseMsg);
+
+                    return next;
+                });
+
+                isInit = true;
             } catch (IOException ex) {
                 throw new RuntimeException(ex);
             }
